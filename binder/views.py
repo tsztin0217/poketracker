ce58f28c0709670d85224ec59459f96088efe41e
tsztin0217 <tsztin0217@gmail.com>
2025-04-29 17:49:09 -0400

    Create BinderList view

--- binder/views.py
+++ binder/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Binder
 
 # Create your views here.
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>PokeTracker Home</h1>')
+
+class BinderList(ListView):
+    model = Binder
