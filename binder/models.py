e9e98306c2ce3551dd4eff2d2cbc915b3df770de
tsztin0217 <tsztin0217@gmail.com>
2025-04-29 17:48:29 -0400

    Create Binder model

--- binder/models.py
+++ binder/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Binder(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
