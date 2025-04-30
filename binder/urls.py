62f48a80e97fe60d9f63db079e681c7a16247112
tsztin0217 <tsztin0217@gmail.com>
2025-04-29 21:02:26 -0400

    Add binder create, detail, and sign up to path

--- binder/urls.py
+++ binder/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('binders/', views.BinderList.as_view(), name='binder-index')
+    path('', views.Home.as_view(), name='home'),
+    path('binders/', views.BinderList.as_view(), name='binder-index'),
+    path('binders/create/', views.BinderCreate.as_view(), name='binder-create'),
+    path('binders/<int:pk>/', views.BinderDetail.as_view(), name='binder-detail'),
+    path('accounts/signup/', views.signup, name='signup'),
+
 ]
