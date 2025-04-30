bf46e07e4320c0908e79e0e439190410e3896a34
tsztin0217 <tsztin0217@gmail.com>
2025-04-29 23:56:16 -0400

    Create card search function

--- binder/views.py
+++ binder/views.py
@@ -7,6 +7,7 @@ from .models import Binder
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .services import fetch_card_data
 
 def signup(request):
     error_message = ''
@@ -41,8 +42,32 @@ class BinderCreate(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         self.object = form.save()
-        return redirect('binder_detail', pk=self.object.pk)
+        return redirect('binder-detail', pk=self.object.pk)
     
 
 class BinderDetail(LoginRequiredMixin, DetailView):
     model = Binder
+
+def card_search(request):
+    query = request.GET.get('query')
+    cards = []
+
+    if query:
+        data = fetch_card_data(query)
+        if data and 'data' in data:
+            for card in data['data']:
+                for card in data['data']:
+                    cards.append({
+                        'id': card['id'],
+                        'name': card.get('name'),
+                        'small_img': card.get('images', {}).get('small'),
+                        'large_img': card.get('images', {}).get('large'),
+                        'set_name': card.get('set', {}).get('name'),
+                        'set_series': card.get('set', {}).get('series'),
+                        'tcg_url': card.get('tcgplayer', {}).get('url')
+                    })
+
+    return render(request, 'search.html', {
+        'cards': cards,
+        'query': query,
+    })
