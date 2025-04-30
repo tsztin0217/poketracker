64da818d244370c15ed35f9d2541cb391344a09f
tsztin0217 <tsztin0217@gmail.com>
2025-04-29 20:48:53 -0400

    Create binder classes and sign up

--- binder/views.py
+++ binder/views.py
@@ -1,12 +1,48 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Binder
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
-from django.http import HttpResponse
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('binder-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+            print(form.errors)  # Debugging print statement
+    
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
 
-def home(request):
-    return HttpResponse('<h1>PokeTracker Home</h1>')
 
-class BinderList(ListView):
+
+class Home(LoginView):
+    template_name = 'binder/home.html'
+
+
+class BinderList(LoginRequiredMixin, ListView):
+    model = Binder
+    template_name = 'binders/index.html'
+
+class BinderCreate(LoginRequiredMixin, CreateView):
+    model = Binder
+    fields = ['name', 'description']
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        self.object = form.save()
+        return redirect('binder_detail', pk=self.object.pk)
+    
+
+class BinderDetail(LoginRequiredMixin, DetailView):
     model = Binder
