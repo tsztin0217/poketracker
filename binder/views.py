1ed7b58ffd47f5273de47088b5974cdbe5c3d497
tsztin0217 <tsztin0217@gmail.com>
2025-04-30 21:14:38 -0400

    Define search card and add card to binder logic

--- binder/views.py
+++ binder/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from .models import Binder
+from .forms import UserCardInfoForm
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -19,7 +20,7 @@ def signup(request):
             return redirect('binder-index')
         else:
             error_message = 'Invalid sign up - try again'
-            print(form.errors)  # Debugging print statement
+            print(form.errors) 
     
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
@@ -48,26 +49,55 @@ class BinderCreate(LoginRequiredMixin, CreateView):
 class BinderDetail(LoginRequiredMixin, DetailView):
     model = Binder
 
-def card_search(request):
-    query = request.GET.get('query')
-    cards = []
 
+
+def search_cards(request, binder_id):
+    print("search_cards function is being called")
+    
+    query = request.GET.get('query') 
+    cards = []  
+    
     if query:
+        query = query.strip()  
         data = fetch_card_data(query)
+        
         if data and 'data' in data:
             for card in data['data']:
-                for card in data['data']:
-                    cards.append({
-                        'id': card['id'],
-                        'name': card.get('name'),
-                        'small_img': card.get('images', {}).get('small'),
-                        'large_img': card.get('images', {}).get('large'),
-                        'set_name': card.get('set', {}).get('name'),
-                        'set_series': card.get('set', {}).get('series'),
-                        'tcg_url': card.get('tcgplayer', {}).get('url')
-                    })
-
-    return render(request, 'search.html', {
+                cards.append({
+                    'id': card['id'],
+                    'name': card.get('name'),
+                    'small_img': card.get('images', {}).get('small'),
+                    'large_img': card.get('images', {}).get('large'),
+                    'set_name': card.get('set', {}).get('name'),
+                    'set_series': card.get('set', {}).get('series'),
+                    'tcg_url': card.get('tcgplayer', {}).get('url')
+                })
+    
+    binder = get_object_or_404(Binder, id=binder_id)
+    
+    return render(request, 'binder/search_cards.html', {
         'cards': cards,
         'query': query,
+        'binder': binder 
+    })
+
+def add_card_to_binder(request, binder_id, card_id):
+    binder = get_object_or_404(Binder, id=binder_id)
+
+    if request.method == 'POST':
+        form = UserCardInfoForm(request.POST)
+        if form.is_valid():
+            user_card = form.save(commit=False)
+            user_card.owner = request.user
+            user_card.binder = binder
+            user_card.card_id = card_id  
+            user_card.save()
+            return redirect('binder-detail', pk=binder_id)
+    else:
+        form = UserCardInfoForm()
+
+    return render(request, 'binder/add_card.html', {
+        'form': form,
+        'binder_id': binder_id,
+        'card_id': card_id
     })
