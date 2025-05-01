18c7a5fe57426749e2fdd929859244792a1b0f17
tsztin0217 <tsztin0217@gmail.com>
2025-05-01 18:16:26 -0400

    Create usercardinfo model

--- binder/models.py
+++ binder/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
+GRADED = (
+    ('', 'Select an option'),
+    ('Y', 'Yes'),
+    ('N', "No")
+)
+
+METHODS = (
+    ('', 'Select a method'),
+    ('E', 'Ebay'),
+    ('S', 'Store'),
+    ('P', 'Pokemon Center'),
+    ('O', 'Other'),
+)
 
 class Binder(models.Model):
     name = models.CharField(max_length=100)
@@ -11,3 +24,23 @@ class Binder(models.Model):
 
     def __str__(self):
         return self.name
+    
+class UserCardInfo(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    binder = models.ForeignKey('Binder', on_delete=models.CASCADE, related_name='cards')
+    card_id = models.CharField(max_length=100) 
+
+    date_obtained = models.DateField(blank=True, null=True)
+    method_obtained = models.CharField(max_length=1, choices=METHODS, blank=True, null=True)
+    graded = models.CharField(
+        max_length=1, 
+        choices=GRADED, 
+        blank=True, 
+        null=True
+        )
+    grade = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    price_paid = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    comments = models.TextField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.card_id} in {self.binder.name}"
