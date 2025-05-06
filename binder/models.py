06bdb5d89ae7a032f8956150efe192a3ed0289d8
tsztin0217 <tsztin0217@gmail.com>
2025-05-06 07:33:39 -0400

    Add last_updated field for binder

--- binder/models.py
+++ binder/models.py
@@ -21,6 +21,7 @@ class Binder(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
