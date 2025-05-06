d705c3dc7b67aa12d1371e7e6604de03165c2382
tsztin0217 <tsztin0217@gmail.com>
2025-05-06 07:50:21 -0400

    Remove base_dir variable to test for error

--- poketracker/settings.py
+++ poketracker/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
